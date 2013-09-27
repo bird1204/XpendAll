50d848990f67af47448c604e47ee03d934b9e559
bird Chiu <bird1204@gmail.com>
2013-09-28 00:42:20 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XpendAll::Application.config.secret_token = '886f7c932dac97fa1fe6833cf2a89829851b5b59b6319c608c2743471e0db6a520905df64aa17866c9a6ac97c2dd24a2538d9a62dd0a4a727db915037bf9c2ca'
